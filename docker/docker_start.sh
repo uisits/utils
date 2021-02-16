c53e0320192e0f3b00bd93f50b300536b4e9f9e0
Vern Huber <workandplay@huberworld.com>
2021-02-16 15:31:31 -0600

    docker utils - eg parse env to csv across all apps

--- docker/docker_start.sh
+++ docker/docker_start.sh
@@ -17,7 +17,7 @@ fi
 #
 sudo docker ps -a | grep -is  $containerID  > /tmp/$$_docker_ps   2>&1
 if [ $? -ne 0 ]; then
-        sudo docker login -u tulio -p dockerrocks  uisdocker1.uisad.uis.edu:8443;
+        sudo docker login -u tulio -p xxxxxx  uisdocker1.uisad.uis.edu:8443;
 	sudo docker pull  uisdocker1.uisad.uis.edu:8443/ubuntu-php-fpm-apache:latest;
 fi 
 
